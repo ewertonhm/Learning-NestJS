9bfc38a36974cd303a8590eacfcd4e0bf24da7bf
ewerton.marschalk <ewerton.marschalk@redeunifique.com.br>
2022-04-07 16:28:09 -0300

    basic nestJS app

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Controller, Module, Get } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
 
 @Controller()
 class AppController {
@@ -6,4 +7,15 @@ class AppController {
     getRootRoute() {
         return 'hi there!';
     }
+}
+
+@Module({
+    controllers: [AppController]
+})
+class AppModule {
+
+}
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
 }
